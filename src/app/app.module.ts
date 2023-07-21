b1b9d77229cd01b6e93bb201a6c6d60bdf29601d
Patrick Schneider <patrick.schneider1412@gmail.com>
2023-07-21 13:03:40 +0200

    New Home Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,13 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+  
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+
 })
 export class AppModule { }
